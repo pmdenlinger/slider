30049165c8ca45d28bc5a0a423fb98f8bca57b83
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 16:43:00 -0700

    Initial Commit

---
+++ slider/sliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  sliderApp.swift
+//  slider
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct sliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
